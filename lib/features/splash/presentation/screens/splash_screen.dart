e6a9b9269cfa296dd79e1be316d807a2ee451e04
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-04-06 09:44:00 +0300

    add splash screen

--- lib/features/splash/presentation/screens/splash_screen.dart
+++ lib/features/splash/presentation/screens/splash_screen.dart
@@ -7,7 +7,7 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SplashScreenBody(),
+      body:  SplashScreenBody(),
     );
   }
 }
