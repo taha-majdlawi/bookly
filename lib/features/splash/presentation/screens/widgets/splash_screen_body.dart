ff850e9349c2c7ff8779ba2abf355475f142bc64
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-04-06 10:56:45 +0300

    Add animation to splash screen

--- lib/features/splash/presentation/screens/widgets/splash_screen_body.dart
+++ lib/features/splash/presentation/screens/widgets/splash_screen_body.dart
@@ -1,8 +1,40 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/home/presentation/screens/home_screen.dart';
+import 'package:bookly_app/features/splash/presentation/screens/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreenBody extends StatelessWidget {
-  const SplashScreenBody({super.key});
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+class SplashScreenBody extends StatefulWidget {
+  const SplashScreenBody({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreenBody> createState() => _SplashViewbodyState();
+}
+
+class _SplashViewbodyState extends State<SplashScreenBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    initSlidingAnimation();
+
+    navigateToHome();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +43,36 @@ class SplashScreenBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsData.logo),
-        Text('Read free books', textAlign: TextAlign.center),
+        const SizedBox(height: 4),
+        SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 2),
+      end: Offset.zero,
+    ).animate(animationController);
+
+    animationController.forward();
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(
+        () => const HomeScreen(),
+        // calculations
+        transition: Transition.fade,
+    //      duration: kTranstionDuration
+      );
+
+      //   GoRouter.of(context).push(AppRouter.kHomeView);
+    });
   }
 }
