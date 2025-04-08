577b0ed0fe491b33f46bae5f72a1cc4a7f896185
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-04-08 10:24:47 +0300

    create app stles

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 
 abstract class Styles {
@@ -7,25 +6,28 @@ abstract class Styles {
     fontWeight: FontWeight.w600,
     fontFamily: 'Mochiy Pop One',
   );
-  // static const textStyle20 = TextStyle(
-  //   fontSize: 20,
-  //   fontWeight: FontWeight.normal,
-  // );
-  // static const textStyle30 = TextStyle(
-  //   fontSize: 30,
-  //   fontWeight: FontWeight.w900,
-  //   fontFamily: kGtSectraFine,
-  //   letterSpacing: 1.2,
-  // );
-  // static const textStyle14 = TextStyle(
-  //   fontSize: 14,
-  //   fontWeight: FontWeight.normal,
-  // );
+  static const textStyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'Mochiy Pop One',
+  );
+  static const textStyle30 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w900,
+    fontFamily: 'Mochiy Pop One',
+    letterSpacing: 1.2,
+  );
+  static const textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'Mochiy Pop One',
+  );
 
-  // static const textStyle16 = TextStyle(
-  //   fontSize: 16,
-  //   fontWeight: FontWeight.w500,
-  // );
+  static const textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Mochiy Pop One',
+  );
 }
 
 const TextStyle textStyle = TextStyle();
