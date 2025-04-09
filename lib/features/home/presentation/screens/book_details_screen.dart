e5275cb209df7987bb0b90339c22b5dee735a51e
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-04-09 08:27:56 +0300

    create BookDetailsScreen

---
+++ lib/features/home/presentation/screens/book_details_screen.dart
@@ -0,0 +1,14 @@
+import 'package:bookly_app/features/home/presentation/screens/widgets/book_details_screen_body.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsScreen extends StatelessWidget {
+  const BookDetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BookDetailsScreenBody(),
+    );
+  }
+}
---
+++ lib/features/home/presentation/screens/widgets/book_details_screen_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class BookDetailsScreenBody extends StatelessWidget {
+  const BookDetailsScreenBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
