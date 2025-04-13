44a177cbd8909e5154324d5ae3b9cb07127ea059
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-04-13 15:02:36 +0300

    add get it backaget and setup service locator

--- lib/features/home/data/repos/home_repo_imol.dart
+++ lib/features/home/data/repos/home_repo_imol.dart
@@ -5,10 +5,10 @@ import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 
-class HomeRepoImol implements HomeRepo {
+class HomeRepoImpl implements HomeRepo {
   final ApiService apiService;
 
-  HomeRepoImol(this.apiService);
+  HomeRepoImpl(this.apiService);
   @override
   Future<Either<Failures, List<BookModle>>> fetchNewestBooks() async {
     try {
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
+import 'package:bookly_app/core/utils/service_locator.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo_imol.dart';
+import 'package:bookly_app/features/home/presentation/manager/featuerd_books_cubit/featuerd_books_cubit.dart';
+import 'package:bookly_app/features/home/presentation/manager/newset_book_cubit/newset_books_cubit.dart';
+import 'package:dio/dio.dart';
 
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(const BooklyApp());
 }
 
@@ -13,10 +21,23 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
-      routerConfig: AppRouter.router,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeatuerdBooksCubit(getIt.get<HomeRepoImpl>()),
+        ),
+        BlocProvider(
+          create: (context) => NewsetBooksCubit(getIt.get<HomeRepoImpl>()),
+        ),
+      ],
+
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+        ),
+        routerConfig: AppRouter.router,
+      ),
     );
   }
 }
