c38856fb9dd811cc682152cb4c5d4297e8e4c23d
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-04-13 13:27:21 +0300

    create newest books cubit files

---
+++ lib/features/home/presentation/manager/newset_book_cubit/newset_books_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'newset_books_state.dart';
+
+class NewsetBooksCubit extends Cubit<NewsetBooksState> {
+  NewsetBooksCubit() : super(NewsetBooksInitial());
+}
---
+++ lib/features/home/presentation/manager/newset_book_cubit/newset_books_state.dart
@@ -0,0 +1,6 @@
+part of 'newset_books_cubit.dart';
+
+@immutable
+sealed class NewsetBooksState {}
+
+final class NewsetBooksInitial extends NewsetBooksState {}
