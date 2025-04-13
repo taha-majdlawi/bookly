318b2aac433933324819a28e08b118e5dce684df
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-04-13 13:28:36 +0300

    create newset books states

--- lib/features/home/presentation/manager/newset_book_cubit/newset_books_cubit.dart
+++ lib/features/home/presentation/manager/newset_book_cubit/newset_books_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly_app/features/home/data/models/book_modle/book_modle.dart';
 import 'package:meta/meta.dart';
 
 part 'newset_books_state.dart';
--- lib/features/home/presentation/manager/newset_book_cubit/newset_books_state.dart
+++ lib/features/home/presentation/manager/newset_book_cubit/newset_books_state.dart
@@ -4,3 +4,15 @@ part of 'newset_books_cubit.dart';
 sealed class NewsetBooksState {}
 
 final class NewsetBooksInitial extends NewsetBooksState {}
+
+final class NewsetBooksSuccess extends NewsetBooksState {
+  final List<BookModle> books;
+  NewsetBooksSuccess(this.books);
+}
+
+final class NewsetBooksFailure extends NewsetBooksState {
+  final String errMsg;
+  NewsetBooksFailure(this.errMsg);
+}
+
+final class NewsetBooksLoading extends NewsetBooksState {}
