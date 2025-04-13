ee99ae1e2301a1ad8899d1301b5beab41882fc25
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-04-13 13:29:55 +0300

    create newest books methode

--- lib/features/home/presentation/manager/newset_book_cubit/newset_books_cubit.dart
+++ lib/features/home/presentation/manager/newset_book_cubit/newset_books_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/models/book_modle/book_modle.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(NewsetBooksLoading());
+
+    var result = await homeRepo.fetchNewestBooks();
+    result.fold(
+      (failure) => emit(NewsetBooksFailure(failure.errMsg)),
+      (books) => emit(NewsetBooksSuccess(books)),
+    );
+  }
 }
