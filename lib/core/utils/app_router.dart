d23ac47e430367c275d42a56947dcee969204e8c
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-04-15 11:10:21 +0300

    add bloc provider where we call the BookDetailsScreen inside AppRoute abstract class

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,9 +1,13 @@
+import 'package:bookly_app/core/utils/service_locator.dart';
 import 'package:bookly_app/features/home/data/models/book_modle/book_modle.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo_imol.dart';
+import 'package:bookly_app/features/home/presentation/manager/simillar_books_cubit/simillar_book_cubit.dart';
 import 'package:bookly_app/features/home/presentation/screens/book_details_screen.dart';
 import 'package:bookly_app/features/home/presentation/screens/home_screen.dart';
 import 'package:bookly_app/features/search/presentation/screens/search_screen.dart';
 import 'package:bookly_app/features/splash/presentation/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
@@ -27,7 +31,10 @@ abstract class AppRouter {
       GoRoute(
         path: kDetailsView,
         builder: (BuildContext context, GoRouterState state) {
-          return  BookDetailsScreen(book: state.extra as BookModle);
+          return BlocProvider(
+            create: (context) => SimillarBookCubit(getIt.get<HomeRepoImpl>()),
+            child: BookDetailsScreen(book: state.extra as BookModle),
+          );
         },
       ),
       GoRoute(
--- lib/features/home/presentation/manager/simillar_books_cubit/simillar_book_cubit.dart
+++ lib/features/home/presentation/manager/simillar_books_cubit/simillar_book_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/models/book_modle/book_modle.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'simillar_book_state.dart';
 
 class SimillarBookCubit extends Cubit<SimillarBookState> {
-  SimillarBookCubit() : super(SimillarBookInitial());
+  SimillarBookCubit(this.homeRepo) : super(SimillarBookInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchSimilarBooks() async {
+    emit(SimillarBookLoading());
+
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) => emit(SimillarBookFailure(failure.errMsg)),
+      (books) => emit(SimillarBookSuccess(books)),
+    );
+  }
 }
